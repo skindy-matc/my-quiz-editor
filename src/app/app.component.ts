8d44f2fddf9df4540fc67e55b097bb9ab852099e
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-04-22 20:00:09 -0500

    post api req to 'save' batch edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -100,6 +100,20 @@ export class AppComponent implements OnInit {
     });
   }
 
+  saveBatchEdits = () => {
+    const editedQuizzes = this.getEditedQuizzes().map(x => ({
+      name: x.name,
+      originalName: x.originalName,
+      questions: x.questions
+    }));
+    const newQuizzes = [];
+
+    this.qSvc.saveQuizzes(editedQuizzes, newQuizzes).subscribe(
+      numberOfChangedQuizzesSaved => console.log(numberOfChangedQuizzesSaved),
+      error => console.log(error)
+    );
+  }
+
   cancelBatchEdits = () => {
     this.loadAllQuizzes();
     this.selectQuiz(undefined);
@@ -110,14 +124,15 @@ export class AppComponent implements OnInit {
   }
 
   get numberOfEditedQuizzes() {
-    return this.quizzes
-      .filter(x => 
-        !x.markedForDelete && 
-        x.originalName !== "Untitled Quiz" && (
-          x.name !== x.originalName || 
-          x.questionsChecksum != x.questions.map(q => q.name).join('~')
-        )
-      ).length;
+    return this.getEditedQuizzes().length;
+  }
+
+  private getEditedQuizzes() {
+    return this.quizzes.filter(
+      x => !x.markedForDelete &&
+      x.originalName !== "Untitled Quiz" && (x.name !== x.originalName ||
+      x.questionsChecksum != x.questions.map(q => q.name).join('~'))
+    );
   }
 
   get numberOfAddedQuizzes() {
--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'; // have to manually update from str
+import { HttpClient, HttpHeaders } from '@angular/common/http'; // have to manually update from str
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +10,27 @@ export class QuizService {
 
   getQuizzes() {
     return this.angularHttpClient.get('https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz');
+  }
+
+  // newQuizzes defaults to empty arr so optional argument
+  saveQuizzes(changedQuizzes: any[], newQuizzes: any[] = []) {
+
+    let h = new HttpHeaders({
+      'Content-Type': 'application/json', 
+      'X-Sas-Token': 'sig=K2WE6NQPtyoV6ke5hwPEaEaW52fgvyFWUeCEdPJls1s'
+    });
+
+    return this.angularHttpClient.post(
+      'https://modern-js.azurewebsites.net/save-quizzes-proxy', 
+      JSON.stringify(
+        {
+          "changedQuizzes": changedQuizzes,
+          "newQuizzes": newQuizzes
+        }
+      ), 
+      {
+        headers: h
+      }
+    );
   }
 }
